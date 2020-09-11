b76542708951e0da277e30773a56cb498b006873
Alexander <shalek.edu@mail.ru>
2020-09-11 18:11:23 +1000

    app(model:user): add logic + profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,12 @@
 class UsersController < ApplicationController
+  before_action :find_user, only: %i[show]
   def new; end
+
+  def show; end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
 end
