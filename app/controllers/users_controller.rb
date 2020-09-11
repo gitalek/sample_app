ae723478c64fbc24b92f6a5523dda00735a36acc
Alexander <shalek.edu@mail.ru>
2020-09-11 19:54:31 +1000

    app: add strong parameters, error output to sign up form; add test on invalid form sumbission

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,27 @@
 class UsersController < ApplicationController
   before_action :find_user, only: %i[show]
+
+  def show; end
+
   def new
     @user = User.new
   end
 
-  def show; end
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #
+    else
+      render 'new'
+    end
+  end
 
   private
+
+  def user_params
+    params.require(:user)
+          .permit(:name, :email, :password, :password_confirmation)
+  end
 
   def find_user
     @user = User.find(params[:id])
